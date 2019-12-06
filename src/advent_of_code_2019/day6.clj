7f15573bf97bf205ecc09f59f6a047f6fe22ee73
Richard Barker <richard.barker@qantashotels.com>
2019-12-07 05:57:35 +1100

    With solution to part 2

--- src/advent_of_code_2019/day6.clj
+++ src/advent_of_code_2019/day6.clj
@@ -3,6 +3,7 @@
             [clojure.test :refer :all]))
 
 (def test-input "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L")
+(def test-input-2 "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN")
 (def file-input (slurp (io/resource "input-day6")))
 
 (defn parse-orbits [orbits]
@@ -11,15 +12,52 @@
                (vec (reverse (clojure.string/split orbit #"\)"))))
              (clojure.string/split-lines orbits))))
 
+(defn path-to-com [orbits planet]
+  (take-while identity
+              (iterate orbits planet)))
+
+(defn path-to-planet [orbits planet path-to-planet]
+  (take-while #(not= path-to-planet %)
+              (iterate orbits planet)))
+
+(defn distance-from-com [orbits planet]
+  (dec
+    (count
+      (path-to-com orbits planet))))
+
 (defn total-orbits [data]
   (let [orbits  (parse-orbits data)
         planets (keys orbits)]
     (reduce
       +
       (map
-        #(dec (count (take-while identity (iterate orbits %))))
+        (partial distance-from-com orbits)
         planets))))
+
+(defn common-orbit [orbits x y]
+  (first
+    (last
+      (take-while
+        #(apply = %)
+        (map vector
+             (reverse (path-to-com orbits x))
+             (reverse (path-to-com orbits y)))))))
+
+(defn orbital-transfers [orbits x y]
+  (let [common (common-orbit orbits x y)]
+    (+ (dec (count (path-to-planet orbits x common)))
+       (dec (count (path-to-planet orbits y common)))
+       )))
 
 (deftest day6
   (is (= 42 (total-orbits test-input)))
-  (is (= 142497 (total-orbits file-input))))
+  (is (= 142497 (total-orbits file-input)))
+  (is (= "D" (common-orbit (parse-orbits test-input-2) "YOU" "SAN")))
+  (is (= 4 (orbital-transfers (parse-orbits test-input-2) "YOU" "SAN")))
+  )
+
+(comment
+
+  (def test-orbits-2 (parse-orbits test-input-2))
+  (orbital-transfers (parse-orbits file-input) "YOU" "SAN")
+  )

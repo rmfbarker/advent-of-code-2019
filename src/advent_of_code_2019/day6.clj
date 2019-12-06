c457f03a5dc75c9c8455e971e992f1fb92750e55
Richard Barker <richard.barker@qantashotels.com>
2019-12-07 05:33:02 +1100

    Refactor. Add tests.

--- src/advent_of_code_2019/day6.clj
+++ src/advent_of_code_2019/day6.clj
@@ -1,27 +1,25 @@
 (ns advent-of-code-2019.day6
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer :all]))
 
-(let [orbits  (into {} (map
-                         (fn [orbit] (vec (reverse (clojure.string/split orbit #"\)"))))
-                         (clojure.string/split-lines "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L")))
-      planets (keys orbits)
-      ]
-  (reduce
-    +
-    (map
-      #(dec (count (take-while identity (iterate orbits %))))
-      planets))
-  )
+(def test-input "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L")
+(def file-input (slurp (io/resource "input-day6")))
 
+(defn parse-orbits [orbits]
+  (into {} (map
+             (fn [orbit]
+               (vec (reverse (clojure.string/split orbit #"\)"))))
+             (clojure.string/split-lines orbits))))
 
-(let [orbits  (into {} (map
-                         (fn [orbit] (vec (reverse (clojure.string/split orbit #"\)"))))
-                         (clojure.string/split-lines (slurp (io/resource "input-day6")))))
-      planets (keys orbits)
-      ]
-  (reduce
-    +
-    (map
-      #(dec (count (take-while identity (iterate orbits %))))
-      planets))
-  )
+(defn total-orbits [data]
+  (let [orbits  (parse-orbits data)
+        planets (keys orbits)]
+    (reduce
+      +
+      (map
+        #(dec (count (take-while identity (iterate orbits %))))
+        planets))))
+
+(deftest day6
+  (is (= 42 (total-orbits test-input)))
+  (is (= 142497 (total-orbits file-input))))
